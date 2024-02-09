c1059f7b9873b071d9def27e8fe12a16d3c64272
massaracsh7 <fatum7@gmail.com>
2024-02-09 16:50:14 +0300

    fix: fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,53 @@
-import { resolve as _resolve } from 'path';
-import { EnvironmentPlugin } from 'webpack';
-import { config } from 'dotenv';
-config();
-import WebpackShellPluginNext from 'webpack-shell-plugin-next';
-import ESLintPlugin from 'eslint-webpack-plugin';
-import NodeTargetPlugin from 'webpack/lib/node/NodeTargetPlugin';
+const path = require('path');
+const {
+  EnvironmentPlugin
+} = require('webpack');
+const {
+  config: dotenvConfig
+} = require('dotenv');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const NodeTargetPlugin = require('webpack/lib/node/NodeTargetPlugin');
 
-let scriptExec;
+dotenvConfig();
 
-if (process.argv[2] === '--mode=production') {
-  scriptExec = 'node dist/server.js';
-} else {
-  scriptExec = 'nodemon dist/server.js --watch';
-}
+const scriptCommand = process.argv[2] === '--mode=production' ?
+  'node dist/index.js' :
+  'nodemon dist/index.js --watch';
 
-
-export const entry = './src/server.ts';
-export const module = {
-  rules: [{
-    test: /\.ts?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  },],
-};
-export const output = {
-  filename: 'server.js',
-  path: _resolve(__dirname, 'dist')
-};
-export const resolve = {
-  extensions: ['.ts', '.js'],
-  alias: {
-    'http': require.resolve('http'),
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [{
+      test: /\.ts?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    }, ],
   },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      'http': require.resolve('http'),
+    },
+  },
+  plugins: [
+    new EnvironmentPlugin(['PORT']),
+    new WebpackShellPluginNext({
+      onBuildEnd: {
+        scripts: [scriptCommand],
+        blocking: false,
+        parallel: true
+      }
+    }),
+    new ESLintPlugin({
+      emitError: true,
+      emitWarning: true,
+      failOnError: true
+    }),
+    new NodeTargetPlugin()
+  ]
 };
-export const plugins = [
-  new EnvironmentPlugin(['PORT']),
-  new WebpackShellPluginNext({
-    onBuildEnd: {
-      scripts: [scriptExec],
-      blocking: false,
-      parallel: true
-    }
-  }),
-  new ESLintPlugin({
-    emitError: true,
-    emitWarning: true,
-    failOnError: true
-  }),
-  new NodeTargetPlugin()
-];
