28b5c0e5be913275fab17ac51ae6d649fe7f9471
massaracsh7 <fatum7@gmail.com>
2024-02-10 09:28:17 +0300

    feat: add dataUsers

---
+++ src/dataUsers.ts
@@ -0,0 +1,41 @@
+import { User, db } from "./types";
+
+export class DataUsers {
+  private database: db;
+
+  constructor() {
+    this.database = [];
+  }
+
+  public add(user: User): User | null {
+    if (this.database.some((item: User) => item.id === user.id)) {
+      return null;
+    }
+    this.database.push(user);
+    return user;
+  }
+
+  public getUsers(): db {
+    return this.database;
+  }
+
+  public getUser(id: string): User | null {
+    return this.database.find((user: User) => user.id === id) ?? null;
+  }
+
+  public update(id: string, updatedUser: User): User | null {
+    const userToUpdate = this.database.find((item: User) => item.id === id);
+    if (!userToUpdate) return null;
+    Object.assign(userToUpdate, updatedUser);
+    return userToUpdate;
+  }
+
+  public delete(id: string | number): User | null {
+    const userToDelete = this.database.find((user: User) => user.id === id);
+    if (!userToDelete) return null;
+    this.database = this.database.filter((user: User) => user.id !== id);
+    return userToDelete;
+  }
+}
+
+export const database = new DataUsers();
